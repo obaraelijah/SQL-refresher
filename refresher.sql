f5bfc7b4ae93a3e90db9328b35c5afd9b9ca3fa4
elijah obara <elijahobara357@gmail.com>
2023-03-12 22:28:23 +0300

    More queries

--- refresher.sql
+++ refresher.sql
@@ -155,3 +155,23 @@ order by created_at desc
 limit 20;
 
 -- posts
+
+select p.title,
+       u.first_name,
+       c.message,
+       u2.first_name comment_creator,
+       f.user_id is not null has_favorited
+from posts p
+inner join users u on p."creatorId" = u.id
+inner join comments c on p.id = c.post_id
+inner join users u2  on u2.id = c.creator_id
+left join favorites f
+    on f.post_id = p.id and f.user_id = 4
+where p.id = 7;
+
+select * from favorites
+where post_id = 7 and user_id = 74;
+
+select count(*) from comments c
+inner join users u2  on u2.id = c.creator_id
+where post_id = 7;
